063cddea16d2e975102fd1086d8749cea19d06bb
Rahmad198 <79825546+Rahmad198@users.noreply.github.com>
2021-07-03 19:20:58 +0700

    Add files via upload

---
+++ views/template/footer.blade.php
@@ -0,0 +1,7 @@
+<body>	
+<div class="wrapper">
+	<div class="footer">
+					<marquee>Maha ID FOOTER</marquee>
+				</div>
+	</div>
+</body>
---
+++ views/welcome.blade.php
@@ -0,0 +1,36 @@
+<body id="page-top">
+
+<!-- Page Wrapper -->
+<div id="wrapper">
+
+    <!-- Sidebar -->
+    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+        <!-- Sidebar - Brand -->
+        <a class="sidebar-brand d-flex align-items-center justify-content-center">
+            <div class="sidebar-brand-icon">
+                <i class="fas fa-store"></i>
+            </div>
+            <div class="sidebar-brand-text mx-3">Klick "Admin" Untuk Masuk Sebagai Admin Maha Id</div>
+        </a>
+
+        <!-- Divider -->
+        <hr class="sidebar-divider my-0">
+
+        <!-- Nav Item - Dashboard -->
+
+        <!-- Divider -->
+        <hr class="sidebar-divider">
+        <!-- Nav Item - Charts -->
+        <li class="nav-item">
+            <a class="nav-link" href="{{route('admin.index')}}">
+                <i class="fas fa-fw fa-baby"></i>
+                <span>ADMIN</span></a>
+        </li>
+
+
+        
+    </ul>
+    <!-- End of Sidebar -->
+            </nav>
+            <!-- End of Topbar -->
